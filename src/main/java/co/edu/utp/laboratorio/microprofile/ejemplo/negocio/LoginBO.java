005f14f0c9e403243d81889d7fe188ec866e742f
Miguel Cortes <andres.cortes@utp.edu.co>
2021-11-21 20:21:13 -0500

    create LoginBO

---
+++ src/main/java/co/edu/utp/laboratorio/microprofile/ejemplo/negocio/LoginBO.java
@@ -0,0 +1,22 @@
+package co.edu.utp.laboratorio.microprofile.ejemplo.negocio;
+
+import co.edu.utp.laboratorio.microprofile.ejemplo.persistencia.entidades.Login;
+import co.edu.utp.laboratorio.microprofile.ejemplo.persistencia.entidades.Persona;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class LoginBO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Login> validateUser(Login login){
+        return entityManager.createNamedQuery(Login.QUERY_VALIDATE_USER,Login.class)
+            .setParameter("usuario",login.getUsuario()).getResultList();
+    }
+
+
+}
